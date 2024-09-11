b0929a03c4401999bd125f3855f35ed3f3ef04cc
Caleb Eden <ceden0207@yahoo.com>
2024-09-11 18:04:40 -0500

    p10k configuration

--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/ins
 sed -i '/^plugins=(/c\plugins=(copypath git history jsontools web-search zsh-syntax-highlighting zsh-autosuggestions)' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i '/^ZSH_THEME="/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+cp (dirname "$0")/p10k.zsh ~/.p10k.zsh
 chsh -s $(which zsh)
 exec zsh
