5d0f5156a6f39345c5d5a599df8aa9f01d0bf573
Caleb Eden <ceden0207@yahoo.com>
2024-09-11 22:07:16 -0500

    Fix chsh

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ cp $(dirname "$0")/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-chsh -s $(which zsh)
+chsh -s $(which zsh) "$USER"
 exec zsh
