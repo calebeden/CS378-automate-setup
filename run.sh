d79949ac5f98754a1883279a1c5fa5bd31dd3afc
Caleb Eden <ceden0207@yahoo.com>
2024-09-11 21:50:02 -0500

    Include entire config files rather than modifying on the fly

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
+set -e
 apt-get update
 apt-get install -y openjdk-17-jdk maven git wget htop tmux unzip bzip2 zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -) --unattended"
-sed -i '/^plugins=(/c\plugins=(copypath git history jsontools web-search zsh-syntax-highlighting zsh-autosuggestions)' ~/.zshrc
+cp $(dirname "$0")/.p10k.zsh ~/.p10k.zsh
+cp $(dirname "$0")/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i '/^ZSH_THEME="/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
-cp $(dirname "$0")/p10k.zsh ~/.p10k.zsh
 chsh -s $(which zsh)
 exec zsh
